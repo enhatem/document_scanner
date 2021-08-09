9b0bbbf4c2a6aa4cd4d5d8b11d0b3879b499e783
Elie Hatem <65036164+enhatem@users.noreply.github.com>
2021-08-10 00:05:27 +0200

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/elie/tutorials/c++/openCV/document_scanner
-# Build directory: /home/elie/tutorials/c++/openCV/document_scanner/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
